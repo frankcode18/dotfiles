aa4fcbbcb43e7459558a7c4b9950ffedc66d95e5
mrmango1 <andersongrefa@gmail.com>
2021-11-30 10:36:42 -0500

    added subfolders

---
+++ qtile/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# nitrogen --restore &
+# feh --bg-scale ~/Pictures/Wallpapers/gruvboxDark.jpg &
+feh --bg-scale ~/Pictures/Wallpapers/retroWaifu.jpg &
+microsoft-edge-stable &
+# xgifwallpaper ~/Pictures/Wallpapers/cyberNeon.gif &
+picom --experimental-backend &
+clipit &
+nm-applet &
+# buckle &
